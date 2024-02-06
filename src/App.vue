1b117fdda13e8b7f5d6dd78ec98903e3d06c5649
salomaxx <teyssier.salome.78@gmail.com>
2024-02-06 10:30:09 +0100

    ajout de la police, modifications dans le header, ajout nouveau favicon

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,21 @@
     <HeaderVue></HeaderVue>
     <div id="app"></div>
 
-    <body>
-    </body>
-
 </template>
 
 <script>
 import HeaderVue from './component/Header.vue';
 
 export default {
-    components: {
-        HeaderVue
-    }
+  components: {
+    HeaderVue
+  }
 }
 </script>
 
-<!--scoped pour dire que ca s'applique que dans le template de ce fichier -->
+<!-- scoped pour dire que ca s'applique que dans le template de ce fichier -->
 <style scoped>
-
- body {
+ /* body {
         margin: 0;
-        font-family: 'Atkinson Hyperlegible', sans-serif;
-    }
-
-header {
-        text-align: center;
-        padding: 0;
-        margin: 0;
-        font-size: clamp(1rem, 2vw, 2.5rem);
-        font-weight: 500;
-        color: #0c0c0c;
-    }
-
-h1 {
-        text-align: center;
-        padding: 0;
-        margin: 0;
-        font-size: clamp(1rem, 2vw, 2.5rem);
-        font-weight: 500;
-        color: #0c0c0c;
-    }
+    } */
 </style>
--- src/component/Header.vue
+++ src/component/Header.vue
@@ -1,11 +1,41 @@
 <template>
-    <header>
-        <h1> TITRE DU SITE</h1>
-    </header>
+    <div class="Header">
+        <body>
+            <header>
+                <h1> The Bojack Horseman Characters</h1>
+            </header>
+        </body>
+    </div>
 
 </template>
 
+<script>
+    import HeaderVue from "./Header.vue"
+</script>
+
 <style>
 
+.Header {
+    text-align: left;
+    margin: 10px;
+}
+
+header{
+    font-family : 'BJPolice' ;
+    font-size : 15px ; 
+}
+
+h1 {
+    color: #0c0c0c;
+    /* padding: 0;
+    margin: 0;
+    font-size: clamp(1rem, 2vw, 2.5rem);
+    font-weight: 100;*/
+    } 
+
+h2 {
+    color: #121212;
+    font-size : 20px ; 
+} 
 
 </style>
