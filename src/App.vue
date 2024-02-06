649c80f217e51eea432a5e768897e51c88aa6562
salomaxx <teyssier.salome.78@gmail.com>
2024-02-06 09:07:00 +0100

    Premier commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    
+    <header>
+        <h1> TITRE DU SITE</h1>
+    </header>
+  
+</template>
+
+<script>
+export default {}
+</script>
+
+<!--scoped pour dire que ca s'applique que dans le template de ce fichier -->
+<style scoped>
+
+ body {
+        margin: 0;
+        font-family: 'Atkinson Hyperlegible', sans-serif;
+    }
+
+header {
+        text-align: center;
+        padding: 0;
+        margin: 0;
+        font-size: clamp(1rem, 2vw, 2.5rem);
+        font-weight: 500;
+        color: #0c0c0c;
+    }
+
+h1 {
+        text-align: center;
+        padding: 0;
+        margin: 0;
+        font-size: clamp(1rem, 2vw, 2.5rem);
+        font-weight: 500;
+        color: #0c0c0c;
+    }
+</style>
