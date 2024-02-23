d310400a7b57623343e9b3cb0a2fa6787fa349c6
salomaxx <teyssier.salome.78@gmail.com>
2024-02-23 18:03:15 +0100

    modif header

--- src/component/Header.vue
+++ src/component/Header.vue
@@ -2,7 +2,7 @@
     <div class="Header">
         <body>
             <header>
-                <br>
+                <br> <!--à modif -->
                 <img src="/Users/salometeyssier/Desktop/web/projet/public/titre-bh.png" alt="characters" style="display: block; margin: 0 auto;" width="90%">
                 <br>
                 <h1> Librairie de tous les personnages apparaissant dans le dessin animé Bojack Horseman</h1>
