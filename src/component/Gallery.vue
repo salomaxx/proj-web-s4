97ff729084507925661ec0a9f4d41886d2d3829e
salomaxx <teyssier.salome.78@gmail.com>
2024-04-10 14:38:24 +0200

    ajout d'un fichier GalleryHeader importé depuis Gallery

--- src/component/Gallery.vue
+++ src/component/Gallery.vue
@@ -1,96 +1,95 @@
 <template>
-    <div>
-      <SearchBar @search="handleSearch" />
-      <div class="character-gallery">
-        <Character v-for="(character, index) in filteredCharacters" :key="index" :characterData="character"/>
-      </div>
+  <div>
+    <GalleryHeader @search="handleSearch" @select="handleSelect" />
+    <div class="character-gallery">
+      <Character v-for="(character, index) in filteredCharacters" :key="index" :characterData="character"/>
     </div>
-  </template>
-  
-  <script>
-  import Character from './Character.vue';
-  import SearchBar from './SearchBar.vue';
-  import { getCharacterData } from '@/services/api/bbAPI.js';
-  
-  const MAX_CHARACTERS = 53; //nombre de perso max sur la page
-  
-  export default {
-    name: 'CharacterGallery',
-    components: {
-      Character,
-      SearchBar
-    },
-    data() {
-      return {
-        characters: [],
-        searchQuery: ''
-      };
-    },
-    computed: {
-      filteredCharacters() {
-        let filtered = this.characters.filter(character => character.occupation);//pour enlever persos dont occupation vide
-        if (this.searchQuery.trim() !== '') {
-          const regex = new RegExp('^' + this.escapeRegExp(this.searchQuery.trim()), 'i');
-          filtered = filtered.filter(character =>
-            regex.test(character.name)
-          );
-        }
-        return filtered.slice(0, MAX_CHARACTERS);
+  </div>
+</template>
+
+<script>
+import Character from './Character.vue';
+import GalleryHeader from './GalleryHeader.vue';
+import { getCharacterData } from '@/services/api/bbAPI.js';
+
+const MAX_CHARACTERS = 53; // nombre de personnages max sur la page
+
+export default {
+  name: 'CharacterGallery',
+  components: {
+    Character,
+    GalleryHeader
+  },
+  data() {
+    return {
+      characters: [],
+      searchQuery: ''
+    };
+  },
+  computed: {
+    filteredCharacters() {
+      let filtered = this.characters.filter(character => character.occupation); // pour enlever les personnages dont l'occupation est vide
+      if (this.searchQuery.trim() !== '') {
+        const regex = new RegExp('^' + this.escapeRegExp(this.searchQuery.trim()), 'i');
+        filtered = filtered.filter(character =>
+          regex.test(character.name)
+        );
       }
-    },
-    methods: {
-      async fetchCharacters() {
-        try {
-          this.characters = await getCharacterData();
-        } catch (error) {
-          console.error('Error fetching characters data:', error);
-        }
-      },
-      handleSearch(query) {
-        this.searchQuery = query;
-      },
-      escapeRegExp(string) {
-        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-      }
-    },
-    mounted() {
-      this.fetchCharacters();
+      return filtered.slice(0, MAX_CHARACTERS);
     }
-  };
-  </script>
-  
-  <style scoped>
+  },
+  methods: {
+    async fetchCharacters() {
+      try {
+        this.characters = await getCharacterData();
+      } catch (error) {
+        console.error('Error fetching characters data:', error);
+      }
+    },
+    handleSearch(query) {
+      this.searchQuery = query;
+    },
+    escapeRegExp(string) {
+      return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
+  },
+  mounted() {
+    this.fetchCharacters();
+  }
+};
+</script>
+
+<style scoped>
+.character-gallery {
+  margin-left: 20%;
+  margin-right: 50px;
+  margin-top: 25px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: .2%;
+}
+
+@media (max-width: 1420px) {
   .character-gallery {
-    margin-left: 20%;
-    margin-right: 50px;
-    margin-top: 25px;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    gap : .2%;
+    grid-template-columns: repeat(4, 1fr);
   }
-  
-  @media (max-width: 1420px) {
-    .character-gallery {
-      grid-template-columns: repeat(4, 1fr);
-    }
+}
+
+@media (max-width: 1150px) {
+  .character-gallery {
+    grid-template-columns: repeat(3, 1fr);
   }
-  
-  @media (max-width: 1150px) {
-    .character-gallery {
-      grid-template-columns: repeat(3, 1fr);
-    }
+}
+
+@media (max-width: 870px) {
+  .character-gallery {
+    grid-template-columns: repeat(2, 1fr);
   }
-  
-  @media (max-width: 870px) {
-    .character-gallery {
-      grid-template-columns: repeat(2, 1fr);
-    }
+}
+
+@media (max-width: 600px) {
+  .character-gallery {
+    grid-template-columns: repeat(1, 1fr);
   }
-  
-  @media (max-width: 600px) {
-    .character-gallery {
-      grid-template-columns: repeat(1, 1fr);
-    }
-  }
-  </style>
-  
+}
+</style>
---
+++ src/component/GalleryHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <SearchBar @search="handleSearch" />
+      <SelectBox @select="handleSelect" />
+    </div>
+  </template>
+  
+  <script>
+  import SelectBox from './SelectBox.vue';
+  import SearchBar from './SearchBar.vue';
+  
+  export default {
+    name: 'GalleryHeader',
+    components: {
+      SelectBox,
+      SearchBar
+    },
+    methods: {
+      handleSearch(query) {
+        this.$emit('search', query);
+      },
+      handleSelect(selection) {
+        this.$emit('select', selection);
+      }
+    }
+  };
+  </script>
+  
