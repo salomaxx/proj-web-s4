c4df3e4697c935b2b60f7c227683fed19594a4ea
salomaxx <teyssier.salome.78@gmail.com>
2024-04-10 14:39:28 +0200

    ajout d'une SelectBox (pas encore fonctionnelle)

---
+++ src/component/SelectBox.vue
@@ -0,0 +1,33 @@
+<template>
+        <select class="selectBox" v-model="selectedOption" @change="selectCards">
+            <option value="all">All</option>
+            <option value="women">Women</option>
+            <option value="men">Men</option>
+        </select>
+</template>
+
+<script>
+    export default {
+    name: 'genderfilter',
+    data() {
+      return {
+        selectedOption: 'all'
+      }
+    },
+    methods: {
+      selectCards() {
+        this.$emit('select', this.selectedOption); 
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+    .selectBox {
+        display: flex;
+        justify-content: center;
+        padding-left: 500px;
+        height: 40px;
+    }
+</style>
